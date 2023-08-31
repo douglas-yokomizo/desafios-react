f239e8d95df5ab5d7c873a8dd0b3e3b62a4f9710
Douglas <yogi.yokomizo@gmail.com>
2023-08-31 08:13:24 -0300

    refactor: merged all states in one

--- 3-events-e-forms/src/components/RegisterForm.tsx
+++ 3-events-e-forms/src/components/RegisterForm.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 
 const RegisterForm = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target // aqui pega o 'name' do input e o 'value'
+        setValues({ ...values, [name]: value }) //esse nome é usado para atualizar as propriedades dinamicamente com o valor do input, por isso é importante ter a propriedade 'name' definida no input
+    }
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        console.log(name, email, password)
+        console.log(values)
     }
 
     return (
@@ -15,11 +22,11 @@ const RegisterForm = () => {
             <h2>1- Formulário de Registro</h2>
             <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '.5rem', marginTop: '1rem' }}>
                 <label htmlFor="nameInput">Name:</label>
-                <input value={name} onChange={(e) => setName(e.target.value)} type="text" id='nameInput' placeholder='Insira seu nome' />
+                <input name='name' value={values.name} onChange={handleChange} type="text" id='nameInput' placeholder='Insira seu nome' />
                 <label htmlFor="emailInput">E-mail:</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id='emailInput' placeholder='Insira seu email' />
+                <input name='email' value={values.email} onChange={handleChange} type="email" id='emailInput' placeholder='Insira seu email' />
                 <label htmlFor="passwordInput">Password:</label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id='passwordInput' placeholder='Insira sua senha' />
+                <input name='password' value={values.password} onChange={handleChange} type="password" id='passwordInput' placeholder='Insira sua senha' />
                 <button type='submit' >Entrar</button>
             </form>
         </>
